c47511df7bc2d7f21f10c0036ea76bb6e259f20e
lupa_or_pupa <hohlov_06@mail.ru>
2019-11-07 19:04:00 +0300

    last final fix

--- messenger/chats/admin.py
+++ messenger/chats/admin.py
@@ -3,19 +3,19 @@ from chats.models import Chat, Attachment, Message, Member
 
 
 class ChatAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'is_group_chat', 'topic', 'last_message')
 
 
 class AttachmentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'user_id', 'chat_id', 'message_id', 'type', 'url')
 
 
 class MessageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'user_id', 'chat_id', 'content', 'added_at')
 
 
 class MemberAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'user_id', 'chat_id', 'new_messages', 'last_read_message_id')
 
 
 admin.site.register(Member, MemberAdmin)
--- messenger/users/admin.py
+++ messenger/users/admin.py
@@ -3,7 +3,7 @@ from users.models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'username', 'password', 'first_name', 'last_name')
 
 
 admin.site.register(User, UserAdmin)
